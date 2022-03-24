512244ec2bd4c642dd0dcd60f55de26a3a1d41ef
alanglais <alix.delanglais@edu.esiee.fr>
2022-03-24 15:14:29 +0100

    ajout de documentation

--- add_word_to_disctionnary.py
+++ add_word_to_disctionnary.py
@@ -1,12 +1,20 @@
 from dictionnary_to_list import dictionnary_to_list
 
-
 def add_word_to_dictionnary(word,file) :
+    """ Ajouter un mot au dictionnaire
+
+    Paramètres 
+    -----------
+    word : le mot que l'on veut ajouter au dictionnaire
+    file : le dictionnaire 
+
+    """
 
     dictionnary=dictionnary_to_list(file)
 
     if (word.lower() not in dictionnary) :
         fichier = open(file, "a")
+        # écriture du mot dans le fichier 
         fichier.write('\n'+ word.lower())
         fichier.close() 
       
