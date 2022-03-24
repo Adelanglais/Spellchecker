281a615d231b07aa55ec5a8bc0a5c96c85fdb7c5
alanglais <alix.delanglais@edu.esiee.fr>
2022-03-24 15:48:09 +0100

    ajout de documentation

--- existing_words.py
+++ existing_words.py
@@ -4,9 +4,28 @@ import numpy as np
 
 def existing_words(word, dictionnary):
 
-    dictionnary_list = np.array(dictionnary_to_list(dictionnary))
-    words_list = np.array(possible_words(word))
+    """Filtrage des corrections 
+
+    Fonctions permettant de ne garder que les combinaisons de caractères
+    présentes dans le dictionnaire
+
+    Paramètres
+    ----------
+    word : le mot à analyser
+    dictionnary : le dictionnaire
+
+    Returns
+    -------
+    list 
+        la liste des corrections du mot qui existent dans le dictionnaire
+        
     
+    """
+    # importation du dictionnaire sous forme de liste
+    dictionnary_list = np.array(dictionnary_to_list(dictionnary))
+    # liste des combinaisons possibles
+    words_list = np.array(possible_words(word))
+    # comparaison des deux listes 
     final_liste = np.intersect1d(words_list,dictionnary_list)
     
     # Résultat
