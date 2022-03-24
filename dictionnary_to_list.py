90224aa575a2af3fb1d218f7de87160eea3096b0
alanglais <alix.delanglais@edu.esiee.fr>
2022-03-24 15:19:57 +0100

    ajout documentation

--- dictionnary_to_list.py
+++ dictionnary_to_list.py
@@ -1,10 +1,24 @@
-
 def dictionnary_to_list(file) :
+    
+    """ Importation du dictionnaire sous forme d'une liste de mots
+
+    Paramètres 
+    -----------
+    file : le dictionnaire
+
+    Returns
+    -------
+    list
+        une liste contenant l'ensemble des mots du dictionnaire
+
+    """
+
     final_list = []
     with open(file, 'r') as f :
         lignes = f.readlines()
         for ligne in lignes :
             final_list.append((ligne.strip('\n')).lower())
+    
     # Résultat
     return final_list
 
