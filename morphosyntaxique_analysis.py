c0b4ce0b110d0892885f14259025dc06b302d36c
alanglais <alix.delanglais@edu.esiee.fr>
2022-04-04 13:58:47 +0200

    analyse morphosyntaxique

--- morphosyntaxique_analysis.py
+++
@@ -1,7 +0,0 @@
-"""Vous écrirez une fonction qui permettra de faire une analyse morphosyntaxique d’un texte fourni en entrée. 
-La sortie de la fonction contiendra, de manière structurée :
-- chaque token
-- le lemme
-- la catégorie grammaticale 
-- éventuellement, la sous-catégorie grammaticale qui leur sont associés.
-"""
